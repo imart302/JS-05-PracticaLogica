3fcd67671c3f0aad6df379a72527d40d558bdd71
Ivan Martinez <imartinezchin@gmail.com>
2023-09-21 15:21:27 -0600

    fix alarm, add root index

--- Alarm/main.js
+++ Alarm/main.js
@@ -1,6 +1,7 @@
 let seconds;
 let secondsNumber = Number.NaN;
 let error = false;
+let secondCounter = 0;
 
 do {
   seconds = prompt('Set your alarm second: ');
@@ -8,6 +9,12 @@ do {
 
 } while ( Number.isNaN(secondsNumber));
 
+
+const id = setInterval(() => {
+  console.log(`Seconds passed: ${++secondCounter}`)
+}, 1000);
+
 setTimeout(() => {
+  clearInterval(id);
   alert('Your alarm finished');
 }, secondsNumber);
